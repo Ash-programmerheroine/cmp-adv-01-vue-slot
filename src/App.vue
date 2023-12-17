98954d5d16367733ebcc50475b38a44bcf9382e6
Ashrafi <ashrafikhanam.18@gmail.com>
2023-12-17 10:20:59 +0600

    edited

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,13 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <!-- scoped slots -->
+      <template #default="slotProps">
+        <h2>{{slotProps.goal}}</h2>
+        <p>{{ slotProps['another-prop'] }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,12 +21,15 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
+
 
 export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoals,
   },
  data() {
     return {
---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <!-- scoped slots -->
+         <slot :goal="goal" another-prop=".rtyretyry.."></slot>
+        </li>
+    </ul>
+</template>
+<script>
+
+
+export default{
+    data(){
+       return{
+        goals:['Finish the Course', 'Learn Vue']
+        } ;
+    },
+}
+</script>
